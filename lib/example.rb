663dd94edf6e5c3b851e3df5ec8d9c19906028d4
meaghan jennings <meaghanljennings@gmail.com>
2020-07-14 14:25:44 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,14 +1,4 @@
 # Use this space to code along with the readme
 def greeting
   puts "Hello World"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
 end
